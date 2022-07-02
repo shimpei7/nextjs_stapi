3c791f10c29fda3b1be4b36f355b95be8c15a07d
shimpei.w <shimpei.watanabe.7@gmail.com>
2022-07-02 09:26:27 +0900

    second commit

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,7 @@ router.register('tasks', TaskViewSet, basename='tasks')
 urlpatterns = [
     path('list-post/', PostListView.as_view(), name='list-post'),
     path('detail-post/<str:pk>/', PostRetrieveView.as_view(), name='detail-post'),
-    path('list-task/', PostListView.as_view(), name='list-task'),
+    path('list-task/', TaskListView.as_view(), name='list-task'),
     path('detail-task/<str:pk>/', TaskRetrieveView.as_view(), name='detail-task'),
     path('register/', CreateUserView.as_view(), name='register'),
     path('auth/', include('djoser.urls.jwt')),
